4187cda85492a55e31c066168b1bd0e9e45c8e79
HaiHua Shi <772911240@qq.com>
2015-07-25 20:12:21 +0800

    add main file
    just for test

---
+++ DataStruct_Linklist.cpp
@@ -0,0 +1,13 @@
+// DataStruct_Linklist.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    cout <<"the data struct files " <<endl;
+	return 0;
+}
+
